65f190ffc6d92c979934e418e435fb604476ee73
Archis Gore <archis@concurix.com>
2017-09-13 14:42:07 -0700

    Updated rescrambler

--- rescramble.sh
+++ rescramble.sh
@@ -1,7 +1,7 @@
 # Rescramble base
-if [[ "$POLYVERSE_POLYMORPHIC_LINUX_KEY" != "" ]]; then \
+if [ "$POLYVERSE_POLYMORPHIC_LINUX_KEY" != "" ]; then \
   echo "Rescrambling all packages on this image..." && \
-  echo "Using Scrambling key: $POLYVERSE_POLYMORPHIC_LINUX_KEY"
+  echo "Using Scrambling key: $POLYVERSE_POLYMORPHIC_LINUX_KEY" && \
   apt-get update && \
   apt-get install -y curl && \
   curl https://repo.polyverse.io/config/ubuntu/xenial/install | sh -s "$POLYVERSE_POLYMORPHIC_LINUX_KEY" && \
