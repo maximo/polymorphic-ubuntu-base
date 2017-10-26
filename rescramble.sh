127c2b15ca24e25a4ce79b7ce5ab4e9508baf0d9
Morgan Hein <morgan@polverse.io>
2017-10-26 13:59:49 -0700

    updated with new installation script and auth key.

--- rescramble.sh
+++ rescramble.sh
@@ -2,9 +2,8 @@
 if [ "$POLYVERSE_POLYMORPHIC_LINUX_KEY" != "" ]; then \
   echo "Rescrambling all packages on this image..." && \
   echo "Using Scrambling key: $POLYVERSE_POLYMORPHIC_LINUX_KEY" && \
-  apt-get update && \
-  apt-get install -y curl && \
-  curl https://repo.polyverse.io/config/ubuntu/xenial/install | sh -s "$POLYVERSE_POLYMORPHIC_LINUX_KEY" && \
+ apt update && apt install -y curl && \
+ curl https://repo.polyverse.io/install.sh | sh -s "$POLYVERSE_POLYMORPHIC_LINUX_KEY" && \
   apt-get update && apt-get -y --allow-change-held-packages install --reinstall $(dpkg --get-selections | awk '{print $1}') && \
   echo "Scrambling complete."; \
 fi
